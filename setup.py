b1cfd3b0023e7d0d1bbb1f02f4c3a024c6d10963
Dominic Steinhöfel <dominic.steinhoefel@cispa.de>
2021-12-07 16:02:16 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='islearn',
+    version='0.1',
+    packages=['islearn', 'islearn.tests'],
+    url='https://projects.cispa.saarland/c01dost/islearn',
+    license='GNU GPLv3',
+    author='Dominic Steinhoefel',
+    author_email='dominic.steinhoefel@cispa.de',
+    description='ISLearn (Learning ISLa input invariants)'
+)
