1a5bb82801f121accb961d3c33c96312da61f0b2
Dominic Steinhöfel <dominic.steinhoefel@cispa.de>
2022-02-01 10:04:49 +0100

    Added test

--- islearn/tests/test_learner.py
+++ islearn/tests/test_learner.py
@@ -53,7 +53,55 @@ class TestLearner(unittest.TestCase):
             self.assertIn(vars["def_ctx"].n_type, ["<block_statement>", "<declaration>"])
             self.assertEqual("<id>", vars["def"].n_type)
 
-    def test_filter_invariants_csv_colno(self):
+    def test_filter_invariants_simple_csv_colno(self):
+        start = isla.Constant("start", "<start>")
+        elem = NonterminalPlaceholderVariable("elem")
+        num = isla.BoundVariable("num", isla.Constant.NUMERIC_NTYPE)
+        needle_placeholder = NonterminalStringPlaceholderVariable("needle")
+
+        abstract_formula = isla.IntroduceNumericConstantFormula(
+            num,
+            sc.forall(
+                elem,
+                start,
+                sc.count({}, elem, needle_placeholder, num)
+            )
+        )
+
+        raw_inputs = [
+            """1a;\"2   a\";\" 12\"
+4; 55;6
+123;1;1
+""",
+            """  1;2 
+""",
+            """1;3;17
+12;" 123";"  2"
+""",
+        ]
+
+        inputs = [
+            isla.DerivationTree.from_parse_tree(
+                next(EarleyParser(csv.CSV_GRAMMAR).parse(inp)))
+            for inp in raw_inputs]
+
+        result = filter_invariants([abstract_formula], inputs, csv.CSV_GRAMMAR)
+        print(len(result))
+        print("\n".join(map(isla.unparse_isla, result)))
+
+        self.assertEqual(2, len(result))
+        for formula in result:
+            vars = {var.name: var for var in isla.VariablesCollector.collect(formula)}
+            self.assertEqual(vars["elem"].n_type, "<csv-record>")
+
+            needle_values = {
+                cast(isla.SemanticPredicateFormula, f).args[1]
+                for f in isla.FilterVisitor(lambda f: isinstance(f, isla.SemanticPredicateFormula)).collect(formula)}
+            for needle in needle_values:
+                self.assertIn(needle, {"<csv-string-list>", "<raw-field>"})
+
+    def test_filter_invariants_complex_csv_colno(self):
+        # TODO
         start = isla.Constant("start", "<start>")
         elem = NonterminalPlaceholderVariable("elem")
         num = isla.BoundVariable("num", isla.Constant.NUMERIC_NTYPE)
