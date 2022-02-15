9459aa31a4ea0ed0f307578ecbd61a3afd7b219b
Dominic Steinhöfel <dominic.steinhoefel@cispa.de>
2022-02-15 11:37:28 +0100

    Removed Prolog stuff

--- src/islearn/learner.py
+++ src/islearn/learner.py
@@ -1,14 +1,10 @@
 import functools
 import io
-
-from isla.language import set_smt_auto_eval
-from isla.solver import ISLaSolver
-from pathos import multiprocessing as pmp
 import itertools
 import logging
 import os.path
 import pkgutil
-from abc import ABC, ABCMeta
+from abc import ABC
 from typing import List, Tuple, Set, Dict, Optional, cast, Callable, Iterable
 
 import isla.fuzzer
@@ -16,13 +12,15 @@ import z3
 from grammar_graph import gg
 from isla import language, isla_predicates
 from isla.evaluator import evaluate
-from isla.helpers import is_z3_var, is_nonterminal, z3_subst, dict_of_lists_to_list_of_dicts
-from isla.isla_predicates import is_before, BEFORE_PREDICATE, COUNT_PREDICATE, reachable
+from isla.helpers import is_z3_var, z3_subst, dict_of_lists_to_list_of_dicts
+from isla.isla_predicates import reachable
+from isla.language import set_smt_auto_eval
+from isla.solver import ISLaSolver
 from isla.type_defs import Grammar
-from swiplserver import PrologMQI
+from pathos import multiprocessing as pmp
 
-from islearn.helpers import e_assert_present, parallel_all, parallel_any, transitive_closure, mappings, \
-    connected_chains, non_consecutive_ordered_sub_sequences, replace_formula_by_formulas
+from islearn.helpers import parallel_all, connected_chains, non_consecutive_ordered_sub_sequences, \
+    replace_formula_by_formulas
 from islearn.language import NonterminalPlaceholderVariable, PlaceholderVariable, NonterminalStringPlaceholderVariable, \
     parse_abstract_isla, StringPlaceholderVariable, AbstractISLaUnparser
 from islearn.mutation import MutationFuzzer
@@ -614,123 +612,6 @@ class NonterminalStringInCountPredicatesFilter(PatternInstantiationFilter):
         )
 
 
-def generate_candidates_prolog(
-        patterns: Iterable[str | language.Formula],
-        inputs: Iterable[language.DerivationTree],
-        grammar: Grammar) -> Set[language.Formula]:
-    patterns = [
-        parse_abstract_isla(pattern, grammar) if isinstance(pattern, str)
-        else pattern
-        for pattern in patterns]
-
-    var_map: Dict[PlaceholderVariable, str] = {
-        placeholder: placeholder.name.upper()
-        for pattern in patterns
-        for placeholder in get_placeholders(pattern)}
-
-    conjunctive_formulas: Dict[language.Formula, List[language.Formula]] = {
-        pattern: [
-            f for f in e_assert_present(
-                split_cnf(
-                    e_assert_present(
-                        get_quantifier_free_core(pattern),
-                        "Only supporting formulas with one quantifier chain.")),
-                "Only supporting conjunctive quantifier-free core.")]
-        for pattern in patterns
-    }
-
-    translators: Set[PrologTranslator] = set(
-        filter(
-            lambda e: e is not None,
-            [get_translator(formula, grammar)
-             for conjuncts in conjunctive_formulas.values()
-             for formula in conjuncts]))
-
-    candidates: Set[language.Formula] = set()
-    inp: language.DerivationTree
-    pattern: language.Formula
-    for inp, pattern in itertools.product(inputs, patterns):
-        query = [f"inner_node({var})" for ph, var in var_map.items()
-                 if isinstance(ph, NonterminalPlaceholderVariable)]
-        query.extend(get_in_query(pattern, inp.id, var_map))
-        query.extend([
-            translator.query(conjunct, var_map)
-            for conjunct in conjunctive_formulas[pattern]
-            for translator in translators
-            if translator.responsible(conjunct)])
-        query.extend([f"nonterminal({var_map[var]})"
-                      for var in var_map
-                      if isinstance(var, NonterminalStringPlaceholderVariable)])
-
-        result = evaluate_prolog_query(
-            generate_assumptions(grammar, inp, translators),
-            ", ".join(query))
-
-        for instantiation in (result or []):
-            candidate = pattern.substitute_variables({
-                placeholder: language.BoundVariable(
-                    placeholder.name,
-                    inp.get_subtree(inp.find_node(instantiation[variable])).value)
-                for placeholder, variable in var_map.items()
-                if isinstance(placeholder, NonterminalPlaceholderVariable)})
-
-            candidate = candidate.substitute_expressions({
-                placeholder: instantiation[variable]
-                for placeholder, variable in var_map.items()
-                if isinstance(placeholder, NonterminalStringPlaceholderVariable)
-            })
-
-            def substitute_string_placeholders(formula: language.Formula) -> language.Formula | bool:
-                if not isinstance(formula, language.SMTFormula):
-                    return False
-
-                string_placeholders = [
-                    v for v in formula.free_variables()
-                    if isinstance(v, StringPlaceholderVariable)]
-                if not string_placeholders:
-                    return False
-
-                for ph in string_placeholders:
-                    string_inst = instantiation[var_map[ph]]
-                    assert isinstance(string_inst, str)
-
-                    return language.SMTFormula(cast(z3.BoolRef, z3_subst(formula.formula, {
-                        ph.to_smt(): z3.StringVal(string_inst)
-                    })), *[v for v in formula.free_variables() if v not in string_placeholders])
-
-            candidate = language.replace_formula(candidate, substitute_string_placeholders)
-
-            candidates.add(candidate)
-
-    return candidates
-
-
-class PrologTranslator(ABC):
-    def responsible(self, formula: language.Formula) -> bool:
-        raise NotImplementedError()
-
-    def query(self, formula: language.Formula, var_map: Dict[PlaceholderVariable, str]) -> str:
-        raise NotImplementedError()
-
-    def facts(self, tree: language.DerivationTree) -> Set[str]:
-        raise NotImplementedError()
-
-
-def generate_assumptions(
-        grammar: Grammar,
-        inp: language.DerivationTree,
-        translators: Iterable[PrologTranslator]) -> Set[str]:
-    assumptions: Set[str] = (
-            {f"inner_node({tree.id})" for _, tree in inp.paths() if tree.num_children() > 0}
-            | get_in_assumptions(inp)
-            | {f"nonterminal(\"{nonterminal}\")" for nonterminal in grammar})
-
-    for translator in translators:
-        assumptions |= translator.facts(inp)
-
-    return assumptions
-
-
 class InVisitor(language.FormulaVisitor):
     def __init__(self):
         self.result: Set[Tuple[language.Variable, language.Variable]] = set()
@@ -743,75 +624,6 @@ class InVisitor(language.FormulaVisitor):
 
     def handle(self, formula: language.QuantifiedFormula):
         self.result.add((formula.bound_variable, formula.in_variable))
-
-
-def get_in_query(
-        formula: language.Formula,
-        root_id: int,
-        var_map: Dict[PlaceholderVariable, str]) -> List[str]:
-    constant = extract_top_level_constant(formula)
-
-    visitor = InVisitor()
-    formula.accept(visitor)
-
-    query: List[str] = []
-    for bound_variable, in_variable in visitor.result:
-        bv = cast(NonterminalPlaceholderVariable, bound_variable)
-        assert isinstance(bv, NonterminalPlaceholderVariable)
-        if in_variable == constant:
-            iv = root_id
-        else:
-            assert isinstance(in_variable, NonterminalPlaceholderVariable)
-            iv = var_map[in_variable]
-
-        query.append(f"tin({var_map[bv]}, {iv})")
-
-    return query
-
-
-def get_in_assumptions(tree: language.DerivationTree) -> Set[str]:
-    result: List[str] = []
-    stack: List[language.DerivationTree] = [tree]
-    while stack:
-        node = stack.pop()
-        for child in node.children:
-            stack.append(child)
-            result.append(f"in({child.id}, {node.id})")
-
-    result.append("tin(A, B) :- in(A, B)")
-    result.append("tin(A, C) :- in(A, B), tin(B, C)")
-
-    return set(result)
-
-
-def evaluate_prolog_query(assumptions: Set[str], query: str) -> Optional[List[Dict[str, int]]]:
-    with PrologMQI() as mqi:
-        with mqi.create_thread() as prolog_thread:
-            for assumption in assumptions:
-                prolog_thread.query(f"assert(({assumption}))")
-            result = prolog_thread.query(query)
-            if result is False:
-                return None
-
-            return result
-
-
-def split_cnf(formula: language.Formula) -> Optional[List[language.Formula]]:
-    if (isinstance(formula, language.StructuralPredicateFormula) or
-            isinstance(formula, language.SemanticPredicateFormula) or
-            isinstance(formula, language.SMTFormula)):
-        return [formula]
-
-    if isinstance(formula, language.ConjunctiveFormula):
-        result: List[language.Formula] = []
-        for child in formula.args:
-            child_result = split_cnf(child)
-            if child is None:
-                return None
-            result.extend(child_result)
-        return result
-
-    return None
 
 
 def get_placeholders(formula: language.Formula) -> Set[PlaceholderVariable]:
@@ -828,223 +640,10 @@ def get_placeholders(formula: language.Formula) -> Set[PlaceholderVariable]:
     return placeholders
 
 
-def get_quantifier_free_core(formula: language.Formula) -> Optional[language.Formula]:
-    if isinstance(formula, language.QuantifiedFormula):
-        return get_quantifier_free_core(formula.inner_formula)
-    if isinstance(formula, language.NumericQuantifiedFormula):
-        return get_quantifier_free_core(formula.inner_formula)
-
-    visitor = language.FilterVisitor(
-        lambda f: (isinstance(f, language.QuantifiedFormula) or
-                   isinstance(f, language.NumericQuantifiedFormula)))
-    if visitor.collect(formula):
-        return None
-
-    return formula
-
-
 def extract_top_level_constant(candidate):
     return next(
         (c for c in language.VariablesCollector.collect(candidate)
          if isinstance(c, language.Constant) and not c.is_numeric()))
-
-
-class BeforePredicateTranslator(PrologTranslator):
-    def responsible(self, formula: language.Formula) -> bool:
-        return isinstance(formula, language.StructuralPredicateFormula) and formula.predicate == BEFORE_PREDICATE
-
-    def query(
-            self,
-            formula: language.StructuralPredicateFormula,
-            var_map: Dict[PlaceholderVariable, str]) -> str:
-        assert self.responsible(formula)
-
-        arg_1 = formula.args[0]
-        assert isinstance(arg_1, NonterminalPlaceholderVariable)
-        arg_2 = formula.args[1]
-        assert isinstance(arg_2, NonterminalPlaceholderVariable)
-        return f"before({var_map[arg_1]}, {var_map[arg_2]})"
-
-    def facts(self, tree: language.DerivationTree) -> Set[str]:
-        ordered_trees: List[Tuple[language.DerivationTree, language.DerivationTree]] = [
-            (t1, t2) for (p1, t1), (p2, t2)
-            in itertools.product(*[[(path, tree) for path, tree in tree.paths()] for _ in range(2)])
-            if is_before(None, p1, p2)]
-
-        result: List[str] = []
-        for t1, t2 in ordered_trees:
-            result.append(f"before({t1.id}, {t2.id})")
-
-        return set(result)
-
-    def __hash__(self):
-        return hash(type(self).__name__)
-
-    def __eq__(self, other):
-        return isinstance(other, BeforePredicateTranslator)
-
-
-class VariablesEqualTranslator(PrologTranslator):
-    def responsible(self, formula: language.Formula) -> bool:
-        return (isinstance(formula, language.SMTFormula) and
-                formula.formula.decl().kind() == z3.Z3_OP_EQ and
-                len(formula.free_variables()) == 2 and
-                all(isinstance(var, NonterminalPlaceholderVariable) for var in formula.free_variables()) and
-                all(is_z3_var(child) for child in formula.formula.children()))
-
-    def query(self, formula: language.SMTFormula, var_map: Dict[PlaceholderVariable, str]) -> str:
-        assert self.responsible(formula)
-
-        free_vars: List[NonterminalPlaceholderVariable] = cast(
-            List[NonterminalPlaceholderVariable], list(formula.free_variables()))
-
-        return f"eqv({var_map[free_vars[0]]}, {var_map[free_vars[1]]})"
-
-    def facts(self, tree: language.DerivationTree) -> Set[str]:
-        equal_trees: List[Tuple[language.DerivationTree, language.DerivationTree]] = [
-            (t1, t2) for (p1, t1), (p2, t2)
-            in itertools.product(*[[(path, tree) for path, tree in tree.paths()] for _ in range(2)])
-            if str(t1) == str(t2)]
-
-        return {f"eqv({t1.id}, {t2.id})" for t1, t2 in equal_trees}
-
-    def __hash__(self):
-        return hash(type(self).__name__)
-
-    def __eq__(self, other):
-        return isinstance(other, VariablesEqualTranslator)
-
-
-class VariableEqualsStringTranslator(PrologTranslator):
-    def responsible(self, formula: language.Formula) -> bool:
-        return (isinstance(formula, language.SMTFormula) and
-                formula.formula.decl().kind() == z3.Z3_OP_EQ and
-                len(formula.free_variables()) == 2 and
-                any(isinstance(fv, NonterminalPlaceholderVariable) for fv in formula.free_variables()) and
-                any(isinstance(fv, StringPlaceholderVariable) for fv in formula.free_variables()) and
-                all(is_z3_var(child) for child in formula.formula.children()))
-
-    def query(self, formula: language.SMTFormula, var_map: Dict[PlaceholderVariable, str]) -> str:
-        assert self.responsible(formula)
-
-        free_vars: List[PlaceholderVariable] = cast(
-            List[PlaceholderVariable], list(formula.free_variables()))
-
-        npv = next(v for v in free_vars if isinstance(v, NonterminalPlaceholderVariable))
-        spv = next(v for v in free_vars if isinstance(v, StringPlaceholderVariable))
-
-        return f"eqstr({var_map[npv]}, {var_map[spv]})"
-
-    def facts(self, tree: language.DerivationTree) -> Set[str]:
-        result: Set[str] = set()
-
-        for path, subtree in tree.paths():
-            # We aim to ignore substrings. Some can be eliminated by ignoring nodes
-            # which are children of a parent with the same type.
-            parents = [tree.get_subtree(path[:idx]) for idx in range(len(path))]
-            if any(parent.value == subtree.value for parent in parents):
-                continue
-
-            result.add(f'eqstr({subtree.id}, "' + str(subtree).replace('"', '""') + '")')
-
-        return result
-
-    def __hash__(self):
-        return hash(type(self).__name__)
-
-    def __eq__(self, other):
-        return isinstance(other, VariableEqualsStringTranslator)
-
-
-class CountPredicateTranslator(PrologTranslator):
-    def __init__(self, grammar: Grammar):
-        self.grammar = grammar
-        self.graph = gg.GrammarGraph.from_grammar(grammar)
-
-    def reachable(self, from_nonterminal: str, to_nonterminal: str) -> bool:
-        return reachable(self.graph, from_nonterminal, to_nonterminal)
-
-    def responsible(self, formula: language.Formula) -> bool:
-        return isinstance(formula, language.SemanticPredicateFormula) and formula.predicate == COUNT_PREDICATE
-
-    def query(
-            self,
-            formula: language.SemanticPredicateFormula,
-            var_map: Dict[PlaceholderVariable, str]) -> str:
-        assert self.responsible(formula)
-
-        args = [
-            var_map[arg] if isinstance(arg, PlaceholderVariable) else
-            (f'"{arg}"' if isinstance(arg, str)
-             else "_")
-            for arg in formula.args]
-        return f'count({", ".join(args)})'
-
-    def facts(self, tree: language.DerivationTree) -> Set[str]:
-        result: List[str] = []
-
-        for _, subtree in tree.paths():
-            if not subtree.children:
-                continue
-            assert is_nonterminal(subtree.value)
-
-            # Only consider subtrees with nonterminal symbols that can occur a variable amount
-            # of times in derivations form the grammar.
-            if all(not self.reachable(other_nonterminal, other_nonterminal) or
-                   not self.reachable(other_nonterminal, subtree.value)
-                   for other_nonterminal in self.grammar):
-                continue
-
-            occurrences: Dict[str, int] = {}
-
-            for nonterminal in self.grammar:
-                # Consider only nonterminals that are reachable from this subtree.
-                if not self.reachable(subtree.value, nonterminal):
-                    continue
-
-                # Furthermore, consider only those which can occur different numbers of times in derivations from
-                # the subgrammar defined by the nonterminal of this subtree. This is the case if there is some
-                # recursive other nonterminal reachable form the nonterminal of this subtree, from which this
-                # nonterminal can be reached.
-                if any(self.reachable(subtree.value, other_nonterminal) and
-                       self.reachable(other_nonterminal, other_nonterminal) and
-                       self.reachable(other_nonterminal, nonterminal)
-                       for other_nonterminal in self.grammar):
-                    occurrences[nonterminal] = 0
-
-            def action(_: language.Path, subsubtree: language.DerivationTree):
-                nonlocal occurrences
-                if subsubtree.children and subsubtree.value in occurrences:
-                    occurrences[subsubtree.value] += 1
-
-            subtree.traverse(action)
-
-            result.extend([
-                f'count({subtree.id}, "{nonterminal}", {occs})'
-                for nonterminal, occs in occurrences.items()])
-
-        return set(result)
-
-    def __hash__(self):
-        return hash(type(self).__name__)
-
-    def __eq__(self, other):
-        return isinstance(other, CountPredicateTranslator)
-
-
-def get_translator(formula: language.Formula, grammar: Grammar) -> Optional[PrologTranslator]:
-    translators = [
-        BeforePredicateTranslator(),
-        CountPredicateTranslator(grammar),
-        VariablesEqualTranslator(),
-        VariableEqualsStringTranslator(),
-    ]
-
-    try:
-        return next(translator for translator in translators
-                    if translator.responsible(formula))
-    except StopIteration:
-        return None
 
 
 def patterns_from_file(file_name: str = STANDARD_PATTERNS_REPO) -> Dict[str, language.Formula]:
